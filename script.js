725c715a81325913c9adb6d5421a77534e0c9a0c
Magnus Bakke <magnusbakke@live.com>
2019-03-27 14:55:23 +0100

    Add compatibility with different browsers

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+=======
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+>>>>>>> 57936a2... Remove empty onload function
 function sendData() {
 	var url_vars = getUrlVars()
 	// authentication_data
@@ -21,9 +48,15 @@ function sendData() {
 		generation_options: JSON.stringify(generation_options)
 	}
 	
-    var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'http://127.0.0.1:8000/connect/');
+    var xhr = createCORSRequest('POST', 'http://127.0.0.1:8000/connect/');
+	
+	if (!xhr) {
+		throw new Error('CORS not supported');
+	}
+	
 	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.withCredentials = true;
+
 	xhr.onload = function() {
 		if (xhr.status === 201) {
 			alert(xhr.responseText);
@@ -34,6 +67,9 @@ function sendData() {
 		else if (xhr.status == 500) {
 			alert('Unexpected error')
 		}
+	};
+	xhr.onerror = function() {
+		alert('Unexpected error. Please check your browser.');
 	};
 	xhr.send(JSON.stringify(data));
 }
